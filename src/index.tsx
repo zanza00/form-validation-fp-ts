3b1a302d1e9540ba6358a0334a2a090ca03afadf
Simone Picciani <spicciani@cuebiq.com>
2020-01-03 14:59:16 +0100

    some basic types to make it compile

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,12 @@ class App extends React.Component<{}, { hasError: boolean }> {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError(error: Error) {
     // Update state so the next render will show the fallback UI.
     console.log("getDerivedStateFromError", error);
     return { hasError: true };
   }
-  componentDidCatch(error, errorInfo) {
+  componentDidCatch(error: any, errorInfo: any) {
     // You can also log the error to an error reporting service
     console.log(error, errorInfo);
   }
